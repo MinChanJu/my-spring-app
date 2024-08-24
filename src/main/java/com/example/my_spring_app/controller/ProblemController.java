140ff9ff689849bd883e26c1b38b213c5cbfdafb
MinChanJu <mcj00220077@gmail.com>
2024-08-24 17:44:16 +0900

    url 허용

--- src/main/java/com/example/my_spring_app/controller/ProblemController.java
+++ src/main/java/com/example/my_spring_app/controller/ProblemController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://minchanju.github.io"})
 @RequestMapping("/api/problems")
 public class ProblemController {
 
