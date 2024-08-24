ebd64a0a570625618702c945945724ea50c74af3
MinChanJu <mcj00220077@gmail.com>
2024-08-24 18:12:21 +0900

    cors 설정

--- src/main/java/com/example/my_spring_app/controller/ProblemController.java
+++ src/main/java/com/example/my_spring_app/controller/ProblemController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "https://minchanju.github.io"})
 @RequestMapping("/api/problems")
 public class ProblemController {
 
