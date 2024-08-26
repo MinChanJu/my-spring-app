00a978fdf0d2aa982b617f4c7dcaa355b8866b3b
MinChanJu <mcj00220077@gmail.com>
2024-08-26 20:27:34 +0900

    유저 생성 변경

--- src/main/java/com/example/my_spring_app/service/UserService.java
+++ src/main/java/com/example/my_spring_app/service/UserService.java
@@ -3,18 +3,44 @@ package com.example.my_spring_app.service;
 import com.example.my_spring_app.model.User;
 import com.example.my_spring_app.model.UserDTO;
 import com.example.my_spring_app.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public static boolean isPasswordValid(String password) {
+        if (Pattern.compile("[\\s'\";]+").matcher(password).find()) {
+            return false;
+        }
+
+        if (Pattern.compile("\\p{Cntrl}").matcher(password).find()) {
+            return false;
+        }
+
+        if (!password.matches(".*[a-zA-Z]+.*")) {
+            return false;
+        }
+
+        if (!password.matches(".*[0-9]+.*")) {
+            return false;
+        }
+
+        if (password.length() < 8) {
+            return false;
+        }
+
+        return true;
+    }
 
     public List<UserDTO> getAllUsers() {
         List<User> users = userRepository.findAll();
@@ -33,7 +59,14 @@ public class UserService {
     }
 
     public User createUser(User user) {
-        return userRepository.save(user);
+        if (userRepository.existsByUserId(user.getUserId())) {
+            return null;
+        } else {
+            if (isPasswordValid(user.getUserPw())) {
+                return userRepository.save(user);
+            }
+            return null;
+        }
     }
 
     public User updateUser(Long id, User userDetails) {
